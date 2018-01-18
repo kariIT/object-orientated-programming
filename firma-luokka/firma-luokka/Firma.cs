da2e2cfa31f1940ef06d17b720f583714d7b0702
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-01-18 20:44:36 +0200

    firma-luokka -printtaa yrityksen tuloksen ja voittoprosentin

---
+++ firma-luokka/firma-luokka/Firma.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firma_luokka
+{
+    class Firma
+    {
+        //Muuttujat
+        public string Nimi;
+        public string Osoite;
+        public int Puhelin;
+        public double Tulot;
+        public double Menot;
+
+        //Konstruktori
+
+        public Firma()
+        {
+            Nimi = "Firma";
+            Osoite = "Sijainti";
+            Puhelin = 1234560000;
+            Tulot = 10000;
+            Menot = 5000;
+
+        }
+
+        //KopioKonstruktori
+
+        public Firma(Firma kopioFirma)
+        {
+            Nimi = kopioFirma.Nimi;
+            Osoite = kopioFirma.Osoite;
+            Puhelin = kopioFirma.Puhelin;
+            Tulot = kopioFirma.Tulot;
+            Menot = kopioFirma.Menot;
+
+        }
+
+        //KopioTapaus
+
+        public Firma(string nimi, string osoite, int puhelin, double tulot, double menot)
+        {
+            Nimi = nimi;
+            Osoite = osoite;
+            Puhelin = puhelin;
+            Tulot = tulot;
+            Menot = menot;
+
+        }
+
+        public string Tiedot()
+        {
+            return $"{Nimi} + {Osoite} + {Puhelin} + {Tulot} + {Menot}";
+            
+        }
+            
+        //Metodit 
+
+        public void Tulos()
+        {
+            double tulos = Tulot - Menot;
+            double voittoprosentti = ((Tulot - Menot) / Menot * 100);
+            
+            
+            Console.WriteLine($"Yrityksen {Nimi} tulos: " + tulos + " ja voitto%: " + voittoprosentti);
+            //tulokseen valuutta ja voittoprosentti näyttämään %arvo yhden desimaalin tarkkuudella
+        }
+    }
+}
+ 
---
+++ firma-luokka/firma-luokka/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firma_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Firma firma = new Firma("Hese", "Kerrostie 2", 0201234567, 10000, 9000);
+            Firma firma2 = new Firma("Näkkäri", "Ranskakuja 5", 0207654321, 20000, 17500);
+
+            firma.Tiedot();
+            firma2.Tiedot();
+
+            firma.Tulos();
+            firma2.Tulos();
+
+            Console.ReadLine();
+        }
+    }
+}
+/*
+
+Kirjoita luokalle
+oletusmuodostin(parametritön),
+muodostin, joka sisältää kaikki parametrit sekä
+kopiontimuodostin, jolla olio voidaan kopioida.
+Määritä luokalle myös metodi, joka tulojen ja menojen perusteella laskee firman voiton.
+Metodin pitää laskea voitto%=[(tulot - menot)/menot x 100] ja sen perusteella tulostaa tulostaa näytölle viesti, 
+joka kertoo, kuinka hyvin firmalla menee.
+kehnosti, jos voitto on alle 100% suurempi kuin menot
+tyydyttävästi, jos voitto on vähintään 200% suurempi kuin menot
+hyvin, jos voitto on vähintään 300% suurempi kuin menot
+Luo testausohjelmassa oliota Firma-luokasta ja kopioi yksi olio kopiointimuodostimella.
+Luotuasi olioita, kutsu niiden metodi, joka kertoo firman tilanteesta.*/
