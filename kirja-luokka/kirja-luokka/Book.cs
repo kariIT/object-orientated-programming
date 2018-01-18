b8a090d7819c0627b48ccfe44097d12eaf813a39
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-01-18 10:30:19 +0200

    kirja-luokka valmis

---
+++ kirja-luokka/kirja-luokka/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace kirja_luokka
+{
+    class Book
+    {
+        // Muuttujat
+        public string Title;
+        public string _author;
+        public int Id;
+        public double Price;
+
+        // Konstruktori
+        public Book()
+        {
+            Title = "Ei tietoa";
+            _author = "xxxx";
+            Id = 123;
+            Price = 10;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            title = Title;
+            author = _author;
+            id = Id;
+            price = Price;
+
+        }
+
+        // Metodit
+        public void Tiedot()
+        {
+            Console.WriteLine("Kirjan nimi: " + Title);
+            Console.WriteLine("Kijoittanut: " + _author);
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Hinta: " + Price);
+            Console.Write("\n");
+
+
+        }
+        public string Vertailu(Book book)
+        {
+
+            if (this.Price > book.Price)
+                return $"{this.Title} on kalliimpi kuin {book.Title} kirja";
+
+            else if (this.Price < book.Price)
+                return $"{book.Title} on kalliimpi kuin {this.Title} kirja";
+
+            else
+                return "Vertailu epäonnistui. ";
+
+        }
+
+    }
+}
---
+++ kirja-luokka/kirja-luokka/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kirja_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book()
+            {
+                Title = "C#",
+                _author = "Masa",
+                Id = 111,
+                Price = 10
+               
+            };
+            Book book2 = new Book()
+            {
+                Title = "Java",
+                _author = "Pena",
+                Id = 222,
+                Price = 20
+            };
+
+            book.Tiedot();
+            book2.Tiedot();
+
+           Console.WriteLine(book.Vertailu(book2));
+
+
+            
+
+
+
+            Console.ReadLine();
+        }
+    } 
+  
+    
+}
