e63e2434b3ce39c877d7359b647ffaec66d4acae
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-01-26 10:17:12 +0200

    kirja-kirjailija-luokka -aloitettu

---
+++ kirja-kirjailija-luokka/kirja-kirjailija-luokka/Kirja.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kirja_kirjailija_luokka
+{
+    class Kirja
+    {
+        string Nimi;
+        string Kirjailija;
+        string Kustantaja;
+        double Hinta;
+        static string teemanNimi;
+
+        //K
+        public Kirja()
+        {
+            Nimi = " ";
+            Kirjailija = " ";
+            Kustantaja = " ";
+            Hinta = 22;
+            teemanNimi = " ";
+        }
+
+        public Kirja(string nimi, string kirjailija, string kustantaja, double hinta, string teemannimi)
+        {
+            Nimi = nimi;
+            Kirjailija = kirjailija;
+            Kustantaja = kustantaja;
+            Hinta = hinta;
+            teemanNimi = teemannimi;
+        }
+
+        //M
+        public void HaeKirja()
+        {
+            Console.Write("Hae kirjaa: ");
+            string haku = Console.ReadLine();
+
+            if (haku == Nimi)
+            {
+                Console.WriteLine("\nKirjan nimi: " + Nimi);
+                Console.WriteLine("Kirjailija: " + Kirjailija);
+                Console.WriteLine("Kustantaja: " + Kustantaja);
+                Console.WriteLine("Hinta: " + Hinta);
+                Console.WriteLine("Teema: " + teemanNimi);
+            }
+
+            else
+                Console.Write("Haku epäonnistui.");
+        }
+
+        /*public static void VaihdaTeema()
+        {
+            
+        }*/
+
+    }
+        
+    
+
+    
+}
---
+++ kirja-kirjailija-luokka/kirja-kirjailija-luokka/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kirja_kirjailija_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kirja kirja = new Kirja("Aamu", "Eeli Tamminen", "PV", 5.10, "metsä");
+
+            Kirja kirja1 = new Kirja("Teline", "Johannes", "Petteri", 8.70, "lauta");
+
+            Kirja kirja2 = new Kirja("Muki", "Eemeli", "Johannes", 15.10, "kahvi");
+
+            Kirja kirja3 = new Kirja("Pöytä", "Kaisa", "HH", 14, "seinä");
+
+
+            kirja.HaeKirja();
+
+
+            Console.ReadLine();
+        }
+    }
+}
