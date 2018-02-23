e443505aca9c145aaa1dee0eccc07ea3ddb56cdf
Joona Heinikari <joona.heinikari@elisanet.fi>
2018-02-23 08:51:55 +0200

    rajapinta -tulostaa tuotteiden tiedot

---
+++ rajapinta/rajapinta/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rajapinta
+{
+    class Product : IProduct
+    {
+         string Name;
+         double Price;
+         int Quantity;
+
+        public Product()
+        {
+            Name = "Undefined";
+            Price = 10;
+            Quantity = 1;
+
+        }
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price}, {Quantity}";
+        }
+
+        public void GetProduct()
+        {
+            Console.Write("Hae tuotetta: ");
+            string haku = Console.ReadLine();
+
+            if (haku == Name)
+                Console.WriteLine("Haku onnistui.");
+            else
+                Console.WriteLine("Haku epäonnistui.");
+        }
+
+        public void CountValue()
+        {
+            double total = Price * Quantity;
+            Console.WriteLine("Total: " + total);
+        }
+        
+        
+    }
+}
---
+++ rajapinta/rajapinta/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace rajapinta
+{
+    class Program
+    {
+        public static List<Product> products = new List<Product>();
+
+        static void Main(string[] args)
+        {
+
+
+            /* Product product = new Product("Product 1", 10, 1);
+             Product product2 = new Product("Product 2", 10, 5);
+             Product product3 = new Product("Product 3", 20, 2);
+
+             products.Add(product);
+             products.Add(product2);
+             products.Add(product3); */
+
+            //  Console.WriteLine(product.ToString());
+
+            //  product3.CountValue();
+
+            //  product.GetProduct();
+            GenerateData();
+            PrintData();
+           
+
+             
+            Console.ReadLine();
+
+        }
+        public static void GenerateData()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                products.Add(new Product("Product "+i, rnd.Next(5, 50), rnd.Next(1, 10)));
+            }  
+        }
+        public static void PrintData()
+        {
+            foreach (var p in products)
+            {
+                Console.WriteLine(p.ToString());
+            }
+        }
+    }
+}
